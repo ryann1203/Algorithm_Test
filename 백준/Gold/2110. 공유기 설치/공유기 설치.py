5d76920a46be41f29aed40543628a9bf104cec3b
ryann1203 <130066603+ryann1203@users.noreply.github.com>
2026-02-13 18:13:21 +0900

    [Gold IV] Title: 공유기 설치, Time: 348 ms, Memory: 41140 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,6 +1,5 @@
-# mid값 = "가장 인접한 두 공유기 사이의 거리"
-# low (최소 거리) = 1 (가장 가까운 두 집 사이의 최소 거리)
-# high (최대 거리) = 맨 끝 집과 맨 첫 집 사이의 거리
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = [0]*n
@@ -8,11 +7,8 @@ for i in range(n):
     arr[i] = int(input())
 
 arr.sort()
-
-#low와 high는 간격 mid의 최소, 최댓값.
 low = 1; high = arr[-1] - arr[0];
 
-# "간격이 mid일 때 몇 대 설치 가능할까?" 라는 질문을 매번 새롭게 던지는 과정.
 while low <= high:
     last_pos = arr[0]
     cnt = 1
@@ -26,7 +22,7 @@ while low <= high:
     if cnt < m:
         high = mid - 1
         
-    else: #cnt가 m보다 큼. 즉 공유기를 충분히 설치함. -> 간격을 더 늘려보자!
+    else:
         answer = mid
         low = mid + 1
         
