4adb26af7d8bcca58eaeda3d642fe7ac586a1e63
ryann1203 <130066603+ryann1203@users.noreply.github.com>
2026-02-28 01:37:39 +0900

    [Gold IV] Title: 연구소, Time: 1564 ms, Memory: 36448 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -11,10 +11,9 @@ for i in range(n):
     arr.append(list(map(int, input().split())))
     for k in range(m):
         if arr[i][k] == 0:
-            cand.append((i,k)) # 이제 0의 위치들까지 구함.
+            cand.append((i,k))
         elif arr[i][k] == 2:
-            q.append((i,k)) # 바이러스의 위치 q에 저장
-            # q = [(0,0), (1,5), ...]
+            q.append((i,k))
             
 copyarr = [row[:] for row in arr]
 q2 = deque(q)
@@ -23,7 +22,7 @@ def bfs(copyarr, q):
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
     while q:
-        x, y = q.popleft() #바이러스 시작지점
+        x, y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
@@ -34,7 +33,7 @@ def bfs(copyarr, q):
                 
     return copyarr
     
-setup = list(combinations(cand, 3)) # 0의 위치들의 3개 조합들 구함.
+setup = list(combinations(cand, 3))
 
 for i in range(len(setup)):
     
@@ -42,11 +41,10 @@ for i in range(len(setup)):
     cnt = 0
     q = deque(q2)
     
-    for sets in setup[i]: # sets = (0,1), (0,2), ...
-        copyarr[sets[0]][sets[1]] = 1 # 이제 벽 3개 세운 상태.
+    for sets in setup[i]:
+        copyarr[sets[0]][sets[1]] = 1 
         
     ans = bfs(copyarr, q)
-
     for c in ans:
         for i in range(m):
             if c[i] == 0:
